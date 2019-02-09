fa1f092a2eb0fcefafc0e15c64a056794f524e5f
DiamondPurvis <purvisdiamond@gmail.com>
2019-02-09 13:23:41 -0500

    Debugging code to work

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -14,14 +14,14 @@ class TabLink {
     this.cards = document.querySelectorAll(".card");
     } else {
       // else if `all` is false, only select the cards with matching this.tabData values
-    this.cards = document.querySelectorAll(`.card[data-tab=${this.tabData}]`);
+    this.cards = document.querySelectorAll(`.card[data-tab = "${this.tabData}"]`);
     }
 
      // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class. 
      this.cards = Array.from(this.cards).map(card => new TabCard(card));
 
     // Add a click event that invokes this.selectTab
-     this.tabElement.addEventListener("click", () => this.selectTab());
+     this.tabElement.addEventListener("click", (event) => this.selectTab(event));
   }
 
   selectTab(){
@@ -68,4 +68,4 @@ class TabCard {
 
 */
 let tabs = document.querySelectorAll(".tab")
-  .forEach(tab => new TabLink(tab));
+  tabs.forEach(tab => new TabLink(tab));
